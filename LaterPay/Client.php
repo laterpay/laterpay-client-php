bd20fa6444c9007b6b8dc7fc1c5760cd1013de52
Lars Schenk <info@lars-schenk.de>
2015-03-18 15:52:56 +0100

    If servername is localhost try using site_url().
    This will work only when embedded in wordpress.
    It's important to be able to scale - so using SERVER_NAME is not a good idea because on cloud hosting all servers might be localhost.

--- LaterPay/Client.php
+++ LaterPay/Client.php
@@ -719,12 +719,16 @@ class LaterPay_Client
         } else {
             $pageURL = 'http://';
         }
+        $serverName = $_SERVER['SERVER_NAME'];
+        if ( $serverName == 'localhost' and function_exists('site_url')) {
+           $serverName = site_url(); // WP function 
+        }
         if ( ! $ssl && $_SERVER['SERVER_PORT'] != '80' ) {
-            $pageURL .= $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $uri;
+            $pageURL .= $serverName . ':' . $_SERVER['SERVER_PORT'] . $uri;
         } else if ( $ssl && $_SERVER['SERVER_PORT'] != '443' ) {
-            $pageURL .= $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $uri;
+            $pageURL .= $serverName . ':' . $_SERVER['SERVER_PORT'] . $uri;
         } else {
-            $pageURL .= $_SERVER['SERVER_NAME'] . $uri;
+            $pageURL .= $serverName . $uri;
         }
 
         return $pageURL;
